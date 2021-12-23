e82250265808274466f07aee3b94e485c96d989f
Chris Dekker <chris@krakend.nl>
2021-12-23 16:17:53 +0100

    -- new

--- src/time.js
+++ src/time.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const logger = require('winston');
+const moment = require('moment');
 const auth = require('../auth.json');
 
 // Configure logger settings
@@ -8,18 +9,9 @@ logger.add(new logger.transports.Console, {
     colorize: true,
 });
 
-const bot = new Discord.Client();
-
-const users = [
-    "328124330229628949",
-    "256837269984378880",
-    "256746397242621952",
-];
-const privateChannelID = "833008601027182612";
-
 const SERVER_ID = "831180398507327508";
-const WATCH_CHANNEL_ID = "923586623592095784";
-let users_by_id = [
+const CHANNEL_ID = "923586623592095784";
+let users = [
     {
         id: "328124330229628949",
         name: "Chris",
@@ -51,20 +43,17 @@ let timeDB = [
     }
 ];
 
-let lastSend = null;
+const bot = new Discord.Client();
+
+bot.login(auth.token);
 
 bot.once('ready', () => {
     logger.info('Connected');
 
-    bot.user.setActivity("this shitty team", {
-        type: "WATCHING",
-        url: "https://www.youtube.com/watch?v=gkTb9GP9lVI",
-    });
-
     (async () => {
         const guild = await bot.guilds.fetch(SERVER_ID);
 
-        users_by_id = users_by_id.map((user) => {
+        users = users.map((user) => {
             const member = guild.member(user.id);
 
             return {
@@ -73,37 +62,15 @@ bot.once('ready', () => {
             };
         });
     })();
-});
 
-bot.login(auth.token);
-
-bot.on('voiceStateUpdate', (_, e) => {
-    if (
-        e.channel.members.some(member => users.includes(member.id)) &&
-        e.channel.id === privateChannelID
-    ) {
-        logger.info("Someone joined the channel!");
-
-        const channelMembers = e.channel.members.map(member => member.user.id);
-        const filteredUsers = users.filter(user => !channelMembers.includes(user));
-
-        const TEN_MINUTES = 60 * 10000;
-
-        if (!lastSend || ((new Date) - lastSend) > TEN_MINUTES) {
-            lastSend = new Date().getTime();
-
-            filteredUsers.forEach(user => {
-                e.guild.members.fetch(user)
-                    .then(user => {
-                        user.send('kom server');
-                    });
-            });
-        }   
-    }
+    // bot.user.setActivity("this shitty team", {
+    //     type: "WATCHING",
+    //     url: "https://www.youtube.com/watch?v=gkTb9GP9lVI",
+    // });
 });
 
 bot.on('presenceUpdate', (oldMember, newMember) => {
-    const user = users_by_id.find((user) => user.id === oldMember.userID);
+    const user = users.find((user) => user.id === oldMember.userID);
 
     if (
         user &&
@@ -130,7 +97,7 @@ bot.on('presenceUpdate', (oldMember, newMember) => {
             const message = `-------------\nUSER: ${user.name}\nTIME START: ${moment.unix(oldTime.start).format('DD-MM-YYYY H:mm:s')}\nTIME NOW: ${moment().format('DD-MM-YYYY H:mm:s')}\nELAPSED: ${moment(oldTime.start).from(moment().unix(), true)}\nNEW STATUS: ${newMember.status}\n-------------`;
        
             (async () => {
-                const channel = await bot.channels.fetch(WATCH_CHANNEL_ID);
+                const channel = await bot.channels.fetch(CHANNEL_ID);
                 channel.send(message);
             })();
         }
