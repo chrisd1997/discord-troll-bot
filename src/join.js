5e4874db9ffac0b01bc25f11679ab58962b72a9e
Chris Dekker <chris@krakend.nl>
2022-01-30 20:01:51 +0100

    -- removed watch channel

--- src/join.js
+++ src/join.js
@@ -83,8 +83,6 @@ bot.on('voiceStateUpdate', (_, e) => {
         e.channel.members.some(member => users.includes(member.id)) &&
         e.channel.id === privateChannelID
     ) {
-        logger.info("Someone joined the channel!");
-
         const channelMembers = e.channel.members.map(member => member.user.id);
         const filteredUsers = users.filter(user => !channelMembers.includes(user));
 
@@ -103,45 +101,45 @@ bot.on('voiceStateUpdate', (_, e) => {
     }
 });
 
-bot.on('presenceUpdate', (oldMember, newMember) => {
-    if (oldMember) {
-        const user = users_by_id.find((user) => user.id === oldMember.userID);
+// bot.on('presenceUpdate', (oldMember, newMember) => {
+//     if (oldMember) {
+//         const user = users_by_id.find((user) => user.id === oldMember.userID);
 
-        if (
-            user &&
-            (
-                oldMember.status === 'offline' ||
-                oldMember.status === 'online'
-            ) &&
-            (
-                newMember.status === 'offline' ||
-                newMember.status === 'online'
-            )
-        ) {
-            const oldTime = timeDB.find((time) => time.id === oldMember.userID);
+//         if (
+//             user &&
+//             (
+//                 oldMember.status === 'offline' ||
+//                 oldMember.status === 'online'
+//             ) &&
+//             (
+//                 newMember.status === 'offline' ||
+//                 newMember.status === 'online'
+//             )
+//         ) {
+//             const oldTime = timeDB.find((time) => time.id === oldMember.userID);
     
-            if (oldTime.status !== null) {
-                // logger.info("-------------");
-                // logger.info(`USER: ${user.name}`);
-                // logger.info(`TIME START: ${moment.unix(oldTime.start).format('DD-MM-YYYY H:mm:s')}`);
-                // logger.info(`TIME NOW: ${moment().format('DD-MM-YYYY H:mm:s')}`);
-                // logger.info(`ELAPSED: ${moment(oldTime.start).from(moment().unix(), true)}`);
-                // logger.info(`NEW STATUS: ${newMember.status}`);
-                // logger.info("-------------");
+//             if (oldTime.status !== null) {
+//                 // logger.info("-------------");
+//                 // logger.info(`USER: ${user.name}`);
+//                 // logger.info(`TIME START: ${moment.unix(oldTime.start).format('DD-MM-YYYY H:mm:s')}`);
+//                 // logger.info(`TIME NOW: ${moment().format('DD-MM-YYYY H:mm:s')}`);
+//                 // logger.info(`ELAPSED: ${moment(oldTime.start).from(moment().unix(), true)}`);
+//                 // logger.info(`NEW STATUS: ${newMember.status}`);
+//                 // logger.info("-------------");
     
-                const message = `-------------\nUSER: ${user.name}\nTIME START: ${moment.unix(oldTime.start).format('DD-MM-YYYY H:mm:s')}\nTIME NOW: ${moment().format('DD-MM-YYYY H:mm:s')}\nELAPSED: ${moment(oldTime.start).from(moment().unix(), true)}\nNEW STATUS: ${newMember.status}\n-------------`;
+//                 const message = `-------------\nUSER: ${user.name}\nTIME START: ${moment.unix(oldTime.start).format('DD-MM-YYYY H:mm:s')}\nTIME NOW: ${moment().format('DD-MM-YYYY H:mm:s')}\nELAPSED: ${moment(oldTime.start).from(moment().unix(), true)}\nNEW STATUS: ${newMember.status}\n-------------`;
            
-                (async () => {
-                    const channel = await bot.channels.fetch(WATCH_CHANNEL_ID);
-                    channel.send(message);
-                })();
-            }
+//                 (async () => {
+//                     const channel = await bot.channels.fetch(WATCH_CHANNEL_ID);
+//                     channel.send(message);
+//                 })();
+//             }
     
-            timeDB = timeDB.map((time) => 
-                time.id === oldMember.userID
-                    ? { ...time, start: moment().unix(), status: newMember.status }
-                    : time
-            );
-        }
-    }
-});
+//             timeDB = timeDB.map((time) => 
+//                 time.id === oldMember.userID
+//                     ? { ...time, start: moment().unix(), status: newMember.status }
+//                     : time
+//             );
+//         }
+//     }
+// });
